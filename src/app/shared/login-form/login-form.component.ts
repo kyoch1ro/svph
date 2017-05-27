5c1f762317a29ad90189bde48a41e4ae4f508e78
Ron Michael Umayan <umayan.ronmichael@yahoo.com>
2017-05-27 18:10:16 +0800

    Added login validator

--- src/app/shared/login-form/login-form.component.ts
+++ src/app/shared/login-form/login-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { FormGroup, 
+         FormBuilder, 
+         Validators, 
+         FormControl} from '@angular/forms';
+import { LoginValidator } from './login-validator';
+import { Observable }  from 'rxjs/Observable';
 
 @Component({
   selector: 'shrd-login-form',
