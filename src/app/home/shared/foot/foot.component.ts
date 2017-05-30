8861ca0df9d0f235661b5d86a2e244d0d661c213
Ron Michael Umayan <umayan.ronmichael@yahoo.com>
2017-05-30 10:16:55 +0800

    Added brand logo on home component

--- src/app/home/shared/foot/foot.component.ts
+++ src/app/home/shared/foot/foot.component.ts
@@ -3,12 +3,19 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 @Component({
   selector: 'home-foot',
   template: `
-    <div class="d-flex flex-item-1 justify-content-start">asdasd</div>
-    <div class="d-flex flex-item-1 justify-content-end">exxx</div>
+    <div class="d-flex flex-item-1 justify-content-start">
+      <img src="assets/images/brand.png"  class="flex-inline" style="height:60px" alt="">
+      <div class="flex-inline d-flex">
+        <h3 class="m-auto"><i>SurveyPH</i></h3>
+      </div>
+    </div>
+    <div class="d-flex flex-item-1 justify-content-end">
+      <div class="ml-auto">Facebook / Instragram / Twitter</div>
+    </div>
   `,
 })
 export class FootComponent implements OnInit {
-  @HostBinding('class') classes = 'home-foot d-flex align-items-end';
+  @HostBinding('class') classes = 'home-foot d-flex align-items-end p-2';
   constructor() { }
 
   ngOnInit() {
