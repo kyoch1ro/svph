d99ef2ad721c6ebb84271747b2835f91757a249e
Ron Michael Umayan <umayan.ronmichael@yahoo.com>
2017-05-30 08:50:43 +0800

    deleted the navbar on core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
 import { DEV_AUTH_PROVIDERS} from './dev-auth.service';
 import { LoginGuard } from './login.guard';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent],
+  declarations: [],
   exports:[
-    NavbarComponent
+    
   ],
   providers:[
     DEV_AUTH_PROVIDERS,
